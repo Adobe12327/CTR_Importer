04b65a595f9e403617a81ac5ce080b2e5c6c5c47
Adobe <67348954+Adobe12327@users.noreply.github.com>
2022-09-22 20:36:44 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,6 +20,7 @@ def menu_func_import(self, context):
 
 def menu_func_export(self, context):
     self.layout.operator(CTR_Operator.ExportB3D.bl_idname, text="CTR 차량 (.b3d)")
+    self.layout.operator(CTR_Operator.ExportO3D.bl_idname, text="CTR 오브젝트 (.o3d)")
 
 def register():
     bpy.types.Scene.ctr_texture_dir = bpy.props.StringProperty(name="텍스쳐 경로")
@@ -27,6 +28,7 @@ def register():
     bpy.utils.register_class(CTR_Operator.ImportO3D)
     bpy.utils.register_class(CTR_Operator.ImportM3D)
     bpy.utils.register_class(CTR_Operator.ExportB3D)
+    bpy.utils.register_class(CTR_Operator.ExportO3D)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
     bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
@@ -36,6 +38,7 @@ def unregister():
     bpy.utils.unregister_class(CTR_Operator.ImportO3D)
     bpy.utils.unregister_class(CTR_Operator.ImportM3D)
     bpy.utils.unregister_class(CTR_Operator.ExportB3D)
+    bpy.utils.unregister_class(CTR_Operator.ExportO3D)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
     bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
     del bpy.types.Scene.ctr_texture_dir
